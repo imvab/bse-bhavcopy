4464f736641da56a14db58147aa7d5887d5ddc62
Vaibhav Birla <vaibhavbirlavb@gmail.com>
2021-02-11 08:59:35 +0530

    Misc

--- main/bhavApi.py
+++ main/bhavApi.py
@@ -26,7 +26,7 @@ def fetchCSV():
     today = date.today().strftime("%d%m%Y")
     url = "https://www.bseindia.com/download/BhavCopy/Equity/EQ" + \
         today[0:4] + today[6:] + "_CSV.ZIP"
-    # url = "https://www.bseindia.com/download/BhavCopy/Equity/EQ090221_CSV.ZIP"
+    # url = "https://www.bseindia.com/download/BhavCopy/Equity/EQ100221_CSV.ZIP"
     print(url)
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -34,7 +34,7 @@ def fetchCSV():
     z = zipfile.ZipFile(io.BytesIO(r.content))
     z.extractall()
     print('Fetched')
-    # csv_file_url = 'EQ090221.CSV'
+    # csv_file_url = 'EQ100221.CSV'
     csv_file_url = "EQ" + today[0:4] + today[6:] + ".CSV"
     csv_file = open(csv_file_url)
     csv_file_text = csv_file.read()
